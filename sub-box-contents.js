de5688d2452969f172c4fd4976cb13778d810253
unknown <reuben.domondon@gmail.com>
2021-03-24 16:00:52 +0800

    created sub-box-contents.js

---
+++ sub-box-contents.js
@@ -0,0 +1,107 @@
+const SUB_BOX_CONTENTS = {
+	world: {
+		"Africa",
+		"Australia",
+		"Americas",
+		"Asia Pacific",
+		"Europe",
+		"Middle East"
+	},
+	us: {
+		"Education",
+		"The Upshot"
+	},
+	politics: {
+		"Joe Biden",
+		"Trump Impeachment"
+	},
+	business: {
+		"Dealbook",
+		"Economy",
+		"Energy",
+		"Markets",
+		"Media",
+		"Entrepreneurship",
+		"Your Money",
+		"Automobiles"
+	},
+	opinion: {
+		"Op-Ed Columnists",
+		"Editorials",
+		"Op-Ed Contributors",
+		"Letters",
+		"Sunday Review",
+	},
+	tech: {
+		"Personal Tech"
+	},
+	science: {
+		"Environment",
+		"Space & Cosmos"
+	},
+	health: {
+		"Money & Policy",
+		"Health Guide"
+	},
+	sports: {
+		"Baseball",
+		"Basketball: College",
+		"Basketball: N.B.A.",
+		"Football: College",
+		"Football: N.F.L.",
+		"Golf",
+		"Soccer",
+		"Tennis",
+		"Olympics",
+	},
+	arts: {
+		"Art & Design",
+		"Dance",
+		"Movies",
+		"Music",
+		"Pop Culture",
+		"Television",
+		"Theater",
+		"Watching",
+		"Awards Season"
+	},
+	books: {
+		"Best Sellers",
+		"By the Book",
+		"The Book Review",
+		"Book Review Podcast",
+		"Globetrotting"
+	},
+	style: {
+		"Men's Style",
+		"On the Runway",
+		"Love"
+	},
+	food: {
+		"Cooking",
+		"Restaurant Search"
+	},
+	more: {
+		"Reader Center",
+		"Parenting",
+		"Wirecutter",
+		"Live Events",
+		"Games",
+		"Newsletters",
+		"The Learning Network",
+		"Multimedia",
+		"Photography",
+		"Podcasts",
+		"NYT Store",
+		"NYT Wine Club",
+		"School of NYT",
+		"Times Journeys",
+		"TimesMachine",
+		"Subscribe",
+		"Manage Account",
+		"Today's Paper",
+		"Tools & Services",
+		"Jobs",
+		"Corrections"
+	}
+}
